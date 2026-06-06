fb2d6901907429c0808106b41577ca02a252addd
virti7 <virtipanchamia25@gmail.com>
2026-06-06 22:19:12 +0530

    Add sitemap routes

---
+++ frontend/app/sitemap.ts
@@ -0,0 +1,31 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://zip-drop.vercel.app",
+      lastModified: new Date(),
+      priority: 1,
+    },
+    {
+      url: "https://zip-drop.vercel.app/blog",
+      lastModified: new Date(),
+      priority: 0.9,
+    },
+    {
+      url: "https://zip-drop.vercel.app/blog/snapdrop-alternative",
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: "https://zip-drop.vercel.app/blog/wetransfer-alternative",
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+    {
+      url: "https://zip-drop.vercel.app/blog/share-files-between-devices",
+      lastModified: new Date(),
+      priority: 0.8,
+    },
+  ];
+}

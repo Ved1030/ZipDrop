e80d2b0b600e7129d2ea0370fa6e7fe7e94be291
virti7 <virtipanchamia25@gmail.com>
2026-06-06 22:13:20 +0530

    Add blog index page

---
+++ frontend/app/blog/page.tsx
@@ -0,0 +1,27 @@
+export default function BlogPage() {
+  return (
+    <main style={{ maxWidth: "800px", margin: "50px auto", padding: "20px" }}>
+      <h1>ZipDrop Blog</h1>
+
+      <ul>
+        <li>
+          <a href="/blog/snapdrop-alternative">
+            Best Snapdrop Alternative in 2026
+          </a>
+        </li>
+
+        <li>
+          <a href="/blog/wetransfer-alternative">
+            Free WeTransfer Alternative
+          </a>
+        </li>
+
+        <li>
+          <a href="/blog/share-files-between-devices">
+            How to Share Files Between Devices
+          </a>
+        </li>
+      </ul>
+    </main>
+  );
+}
